9c7f82eb43f4dd0ea3d0cf5158cc36f663fc80cc
codeash24 <o.a.aishu@gmail.com>
2023-05-04 21:54:56 +0530

    Revert "add all commits"
    This reverts commit 3df6f84f9de1cffb94a7ff87727bd0aad5545ae3.

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-var water_waves = new Audio()
-water_waves.src = "/SoundSite/src/water_waves.wav"
-
-function audioFunction(audio){
-   if (audio.paused) {
-    // audio.currentTime=0;
-   audio.loop=true;
-    audio.play();
- } else {
-   audio.pause();
- }
-}
+let sound=[src/rain.mp3];
