3df6f84f9de1cffb94a7ff87727bd0aad5545ae3
codeash24 <o.a.aishu@gmail.com>
2023-05-04 21:42:21 +0530

    add all commits

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let sound=[src/rain.mp3];
+var water_waves = new Audio()
+water_waves.src = "/SoundSite/src/water_waves.wav"
+
+function audioFunction(audio){
+   if (audio.paused) {
+    // audio.currentTime=0;
+   audio.loop=true;
+    audio.play();
+ } else {
+   audio.pause();
+ }
+}
